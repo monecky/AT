c8098e8156d6f864ab70809410bb6160a979d2ab
s2386658 <m.ma.p.vaniperen@student.utwente.nl>
2023-11-16 14:39:42 +0100

    Comments on the same level as the other classes.

--- src/attack_tree.py
+++ src/attack_tree.py
@@ -2,26 +2,37 @@ from typing import List
 
 from src.edge import Edge
 from src.node import Node
-
+index = 0
 
 class AttackTree(object):
+    """
+    'AT' objects representation of a complexer hole.
+    """
     def __init__(self, nodes: List[Node], edges: List[Edge]):
+        """
+        Creates a graph.
+        :param nodes: Represent the nodes
+        :param edges: Represent the edges
+        """
+        global index
         self._nodes = nodes
         self._edges = edges
+        self.index = index
+        index += 1
 
     def __repr__(self):
         """
         A programmer-friendly representation of the attack tree.
         :return: The string to approximate the constructor arguments of the 'AT'
         """
-        return 'Attack tree(Nodes={}, Edges={})'.format(self.edges, self.nodes)
+        return 'Attack tree(Nodes={}, Edges={}, index={)'.format(self.edges, self.nodes, self.index)
 
     def __str__(self) -> str:
         """
         A user-friendly representation of this attack tree
         :return: A user-friendly representation of this attack tree
         """
-        return '({}, {})'.format(str(self.nodes), str(self.edges))
+        return '({},{}, {})'.format(str(self.index),str(self.nodes), str(self.edges))
 
     @property
     def nodes(self):
--- src/nodetype.py
+++ src/nodetype.py
@@ -2,6 +2,9 @@ from enum import Enum
 
 
 class NodeType(Enum):
+    """
+    Enum class for all types of Nodes, might get added some versions later on.
+    """
     ROOT = 0
     BAS = 1
     OR = 2
