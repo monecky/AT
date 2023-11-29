8d0c23790567ddb6ac6f3c3d69f6b8ec769064a1
s2386658 <m.ma.p.vaniperen@student.utwente.nl>
2023-11-29 20:54:07 +0100

    intermediate parent node.

--- src/algorithm/bottom_up.py
+++ src/algorithm/bottom_up.py
@@ -12,21 +12,22 @@ def bottom_up_tree(at: 'AttackTree', root: 'Node', semi_ring: SemiRing):
     result = 0
     match root.node_type:
         case NodeType.ROOT_OR:
-            result = semi_ring.or_operator([bottom_up_tree(at, child, semi_ring) for child in root.children])
+            result = semi_ring.root_or_operator([bottom_up_tree(at, child, semi_ring) for child in root.children])
         case NodeType.ROOT_AND:
-            result = semi_ring.and_operator(([bottom_up_tree(at, child, semi_ring) for child in root.children]))
+            result = semi_ring.root_and_operator(([bottom_up_tree(at, child, semi_ring) for child in root.children]))
         case NodeType.OR:
             result = semi_ring.or_operator([bottom_up_tree(at, child, semi_ring) for child in root.children])
         case NodeType.AND:
             result = semi_ring.and_operator(([bottom_up_tree(at, child, semi_ring) for child in root.children]))
         case NodeType.BAS:
             if root.isMultiParent():
-                result = [[root.attribute.value, [root]], [root, root.attribute.value]]
+                result = [[[root.attribute.value, [root]], [root, root.attribute.value]]]
             else:
-                result = [[root.attribute.value, []], [root, root.attribute.value]]
+                result = [[[root.attribute.value, []], [[root, root.attribute.value]]]]
         case _:
             AtError('Not a valid node type.')
     if root.isMultiParent():
-        #TODO
-        map_mp[root] = result
+        for sub_result in result:
+            sub_result[0][1] += [root]
+            sub_result[1] += [[root, result]]
     return result
