944864c99c77e30af6b1158657bae9118d52ce82
s2386658 <m.ma.p.vaniperen@student.utwente.nl>
2023-11-29 20:41:17 +0100

    Improvement of different root or and and

--- src/model/ring/semi_ring.py
+++ src/model/ring/semi_ring.py
@@ -4,6 +4,13 @@ from abc import ABC, abstractmethod
 class SemiRing(ABC):
     def __init(self, field):
         self._field = field
+    @abstractmethod
+    def root_or_operator(self, array_of_numbers):
+        pass
+
+    @abstractmethod
+    def root_and_operator(self, array_of_numbers):
+        pass
 
     @abstractmethod
     def or_operator(self, array_of_numbers):
@@ -18,36 +25,40 @@ class SemiRing(ABC):
         return self._field
 
 
-class MiniumCostMetric(SemiRing):
-    def __init__(self, field):
-        self._field = field
-
-    @classmethod
-    def or_operator(cls, array_of_numbers):
-        return min(array_of_numbers)
-
-    @classmethod
-    def and_operator(cls, array_of_numbers):
-        return sum(array_of_numbers)
-
-
 class MiniumCostMetricTree(SemiRing):
     def __init__(self, field):
         self._field = field
 
     @classmethod
+    def root_or_operator(cls, array_of_numbers):
+        return min(array_of_numbers)
+
+    @classmethod
+    def root_and_operator(cls, array_of_numbers):
+        return sum(array_of_numbers)
+
+
+    @classmethod
     def or_operator(cls, array_of_numbers):
         return min(array_of_numbers)
 
     @classmethod
     def and_operator(cls, array_of_numbers):
         return sum(array_of_numbers)
+
 
 
 class MiniumCostMetricGraph(SemiRing):
     def __init__(self, field):
         self._field = field
 
+    @classmethod
+    def root_or_operator(cls, array_of_numbers):
+        return min(array_of_numbers)
+
+    @classmethod
+    def root_and_operator(cls, array_of_numbers):
+        return sum(array_of_numbers)
     @classmethod
     def or_operator(cls, array_of_numbers):
         return min(array_of_numbers)
