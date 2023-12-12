0db143f64c964836cb8da33b7f47e7dc9c9580a5
s2386658 <m.ma.p.vaniperen@student.utwente.nl>
2023-12-12 22:40:20 +0100

    small changes

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,15 @@
-from src.algorithm.bottom_up import BottomUp
+from src.algorithm.bottom_up_basis import BottomUpAll
 from src.algorithm.generateAllMP import gen_bu2
 from src.model.ring.semi_ring import MiniumCostMetricTree
 from src.reader.reader import *
 from src.algorithm.bottom_up_tree import *
-from src.algorithm.generateAllpossibilities import *
 import os
 
 def main():
     path = "..\src\example_tree\generate\generate100100100100"
     semi = MiniumCostMetricTree("int")
     bottom_up_tree = BottomUpTree()
-    bottom_up = BottomUp()
+    bottom_up = BottomUpAll()
     for file in os.listdir(path):
         filename = os.fsdecode(file)
         if filename.endswith(".at"):
@@ -22,7 +21,8 @@ def main():
             # gen_bu2(at,at.root, semi)
             # print(min([bu[0] for bu in gen_bu2(at, at.root, semi)]))
             if (not (filename.__contains__("23")or filename.__contains__("28") or filename.__contains__("30") or filename.__contains__("35") or filename.__contains__("40"))):
-                # gen1 = bottom_up.run(at, at.root, semi)
+                gen1 = bottom_up.run(at, at.root, semi)
+                print(gen1)
                 gen2 = bottom_up_tree.run(at, at.root, semi)
                 print(gen2)
                 # if min([bu[0] for bu in gen1]) != min([bu[0] for bu in gen2]):
---
+++ src/algorithm/bottom_up_basis.py
@@ -0,0 +1,62 @@
+from src.algorithm.abstractbu import AbstractBu
+from src.model.at.attack_tree import AttackTree
+from src.model.at.node import Node
+from src.model.ring.semi_ring import SemiRing
+
+
+class BottomUpAll(AbstractBu):
+    def action_ROOT_AND(self, at: 'AttackTree', node: 'Node', semi_ring: SemiRing):
+        result = self.action_AND(at, node, semi_ring)
+        return result
+
+    def action_ROOT_OR(self, at: 'AttackTree', node: 'Node', semi_ring: SemiRing):
+        result = self.action_OR(at, node, semi_ring)
+        return result
+
+    def action_AND(self, at: 'AttackTree', node: 'Node', semi_ring: SemiRing):
+        ch_v = [self.run(at, child, semi_ring) for child in node.children]
+        result = [[0, set(), {}]]
+        for u in ch_v:
+            w = []
+            for bu in u:
+                for re in result:
+                    k = [re[0], re[1].copy(), re[2].copy()]
+                    k[0] = semi_ring.and_operator([k[0], bu[0]])
+                    for n in bu[1]:
+                        if n in k[1]:
+                            k[0] = semi_ring.and_operator([k[0], semi_ring.reverse_and(bu[2][n])])
+                        else:
+                            k[1].add(n)
+                            k[2][n] = bu[2][n]
+                    w += [k]
+            result = w
+        return result
+
+    def action_OR(self, at: 'AttackTree', node: 'Node', semi_ring: SemiRing):
+        ch_v = [self.run(at, child, semi_ring) for child in node.children]
+        result = []
+        for u in ch_v:
+            result += u
+        return result
+
+    def action_BAS(self, at: 'AttackTree', node: 'Node', semi_ring: SemiRing):
+        return [[node.attribute.value, {node}, {node: node.attribute.value}]]
+
+    def action_AFTER(self, array_of_numbers, node):
+        return array_of_numbers
+
+    @staticmethod
+    def thin_out(mapping_value):
+        # Thin out options, by choosing the smallest with the nodes.
+        option = []
+        for change in mapping_value:
+            add = True
+            for found in option:
+                if found[1] == change[1]:
+                    add = False
+                    if found[0] > change[0]:
+                        option.remove(found)
+                        option.append(change)
+            if add:
+                option += [change]
+        return option
