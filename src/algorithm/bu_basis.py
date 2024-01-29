f45568a9308c745e571a30cae27c9b051f3a9658
s2386658 <m.ma.p.vaniperen@student.utwente.nl>
2024-01-29 12:00:44 +0100

    Javadoc bu_basisMP.py and bu_basis.py

--- src/algorithm/bu_basis.py
+++ src/algorithm/bu_basis.py
@@ -3,17 +3,45 @@ from src.model.at.attack_tree import AttackTree
 from src.model.at.node import Node
 from src.model.ring.semi_ring import SemiRing
 
+"""
+BuBas algorithm of the paper.
+"""
+
 
 class BuBasis(AbstractBu):
     def action_ROOT_AND(self, at: 'AttackTree', node: 'Node', semi_ring: SemiRing, run):
+        """
+          Return the attribute together with the AND action.
+          :param node: the current node
+          :param at: the attack tree.
+          :param semi_ring: the operators of the attack tree.
+          :param run: methode of bu.
+          :return: the attribute together with the AND action.
+          """
         result = self.action_AND(at, node, semi_ring, run)
         return result
 
     def action_ROOT_OR(self, at: 'AttackTree', node: 'Node', semi_ring: SemiRing, run):
+        """
+           Return the attribute together with the OR action.
+           :param node: the current node
+           :param at: the attack tree.
+           :param semi_ring: the operators of the attack tree.
+           :param run: methode of bu.
+           :return: the attribute together with the OR action.
+           """
         result = self.action_OR(at, node, semi_ring, run)
         return result
 
     def action_AND(self, at: 'AttackTree', node: 'Node', semi_ring: SemiRing, run):
+        """
+           Determine the AND action
+           :param node: the current node
+           :param at: the attack tree.
+           :param semi_ring: the operators of the attack tree.
+           :param run: methode of bu.
+           :return: a list of triple with all possibilities together with the AND operator.
+           """
         ch_v = [run(at, child, semi_ring) for child in node.children]
         result = [[0, set(), {}]]
         for u in ch_v:
@@ -25,12 +53,20 @@ class BuBasis(AbstractBu):
                     new_dict = re[2].copy() | bu[2]
                     k = [semi_ring.and_operator(
                         [re[0], bu[0], semi_ring.reverse_and(semi_ring.and_operator([new_dict[i] for i in delta]))]),
-                         new_set, new_dict]
+                        new_set, new_dict]
                     w += [k]
             result = w
         return result
 
     def action_OR(self, at: 'AttackTree', node: 'Node', semi_ring: SemiRing, run):
+        """
+           Determine the OR action
+           :param node: the current node
+           :param at: the attack tree.
+           :param semi_ring: the operators of the attack tree.
+           :param run: methode of bu.
+           :return: a list of triple with all possibilities together with the OR operator in between them.
+           """
         ch_v = [run(at, child, semi_ring) for child in node.children]
         result = []
         for u in ch_v:
@@ -38,23 +74,39 @@ class BuBasis(AbstractBu):
         return result
 
     def action_BAS(self, at: 'AttackTree', node: 'Node', semi_ring: SemiRing, run):
+        """
+           Determine the BAS action
+           :param node: the current node
+           :param at: the attack tree.
+           :param semi_ring: the operators of the attack tree.
+           :param run: methode of bu.
+           :return: a list of triple that contains the node.
+           """
         return [[node.attribute.value, {node}, {node: node.attribute.value}]]
 
     def action_AFTER(self, array_of_numbers, node: Node, at: AttackTree, run):
+        """
+           Determine the AFTER action
+           :param node: the current node
+           :param at: the attack tree.
+           :param array_of_numbers: temp result.
+           :param run: methode of bu.
+           :return: array_of_numbers
+           """
         return array_of_numbers
 
-    @staticmethod
-    def thin_out(mapping_value):
-        # Thin out options, by choosing the smallest with the nodes.
-        option = []
-        for change in mapping_value:
-            add = True
-            for found in option:
-                if found[1] == change[1]:
-                    add = False
-                    if found[0] > change[0]:
-                        option.remove(found)
-                        option.append(change)
-            if add:
-                option += [change]
-        return option
+    # @staticmethod
+    # def thin_out(mapping_value):
+    #     # Thin out options, by choosing the smallest with the nodes.
+    #     option = []
+    #     for change in mapping_value:
+    #         add = True
+    #         for found in option:
+    #             if found[1] == change[1]:
+    #                 add = False
+    #                 if found[0] > change[0]:
+    #                     option.remove(found)
+    #                     option.append(change)
+    #         if add:
+    #             option += [change]
+    #     return option
--- src/algorithm/bu_basisMP.py
+++ src/algorithm/bu_basisMP.py
@@ -3,12 +3,30 @@ from src.model.at.attack_tree import AttackTree
 from src.model.at.node import Node
 from src.model.ring.semi_ring import SemiRing
 
+"""BuMp algorithm from the paper"""
+
 
 class BuBasisMP(BuBasis):
     def action_BAS(self, at: 'AttackTree', node: 'Node', semi_ring: SemiRing, run):
+        """
+        returns a triple with attribute value
+        :param at: the attack tree
+        :param node: the node that is considered
+        :param semi_ring: the semi-ring that is used
+        :param run: the bottom-up algorithm that is used.
+        :return: a triple with attribute value on place.
+        """
         return [[node.attribute.value, set(), dict()]]
 
     def action_AFTER(self, array_of_numbers, node: Node, at: AttackTree, run):
+        """
+        add if necessary the mp
+        :param array_of_numbers: result
+        :param node:  node that is considered
+        :param at: the attack tree
+        :param run: the bottom-up algorithm
+        :return: the mp is added if node is mp, to each triple.
+        """
         if node.isMultiParent():
             for re in array_of_numbers:
                 alpha = re[0]
